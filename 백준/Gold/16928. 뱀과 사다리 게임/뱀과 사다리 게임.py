8cd9b835718cf159ee907751ae839f6ab3f22bc4
yhyeonseo <154505487+franktome@users.noreply.github.com>
2024-07-12 02:24:38 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 124 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -20,19 +20,27 @@ queue=deque([1])
 def bfs():
     while queue:
         x = queue.popleft()
-        for dx in range(1, 7):
-            nx = x + dx
-            if nx <= 100:
-                if nx in ladder:
-                    nx = ladder[nx]
-                elif nx in snake:
-                    nx = snake[nx]
+        for dx in range(1,7):
+            nx = x+dx
+            if nx <= 100 and grape[nx]==-1:
+                grape[nx] = grape[x]+1
+                if nx==100:
+                    return
 
-                if grape[nx] == -1:
-                    grape[nx] = grape[x] + 1
+                if nx in ladder:
+                    if grape[ladder[nx]] != -1:
+                        grape[ladder[nx]] = min(grape[nx], grape[ladder[nx]])
+                    else:
+                        grape[ladder[nx]] = grape[nx]
+                    queue.append(ladder[nx])
+                elif nx in snake:
+                    if grape[snake[nx]]!=-1:
+                        grape[snake[nx]] = min(grape[nx], grape[snake[nx]])
+                    else:
+                        grape[snake[nx]] = grape[nx]
+                    queue.append(snake[nx])
+                else:
                     queue.append(nx)
-                    if nx == 100:
-                        return
 
 bfs()
 print(grape[100])
