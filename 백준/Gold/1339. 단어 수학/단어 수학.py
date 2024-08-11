8a749b36eaa780fd3b6f58a23152f765e764e5b0
yhyeonseo <154505487+franktome@users.noreply.github.com>
2024-08-11 10:49:00 +0900

    [Gold IV] Title: 단어 수학, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.py
+++ 백준/Gold/1339. 단어 수학/단어 수학.py
@@ -1,21 +1,23 @@
 import sys
 
-n=int(sys.stdin.readline())
+n= int(sys.stdin.readline())
 words=[sys.stdin.readline().strip() for _ in range(n)]
 
-dic = {}
+dic={}
 for word in words:
     x=len(word)-1
-    for i in word:
-        if i in dic:
-            dic[i] += 10 ** x
+    for j in word:
+        if j in dic:
+            dic[j]+=10**x
         else:
-            dic[i]= 10 ** x
+            dic[j]=10**x
         x-=1
-dic1=sorted(dic.values(),reverse=True)
+
+dic2 = sorted(dic.values(),reverse=True)
+
 k=9
 sum=0
-for i in dic1:
-    sum += i*k
+for i in dic2:
+    sum+=i*k
     k-=1
 print(sum)
