1e5916e0a9b269c255468bdbc7405d3e3e886350
yhyeonseo <154505487+franktome@users.noreply.github.com>
2024-08-21 17:49:09 +0900

    [Gold V] Title: 개똥벌레, Time: 152 ms, Memory: 118344 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.py
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -1,47 +1,28 @@
+# 누적합을 이용한 풀이
 import sys
 
-n, h = map(int, sys.stdin.readline().split())
-ground=[]
-sky=[]
+n,h = map(int,sys.stdin.readline().split())
+
+ground=[0]*(h+1)
+cell = [0]*(h+1)
 for i in range(n):
+    size = int(sys.stdin.readline())
     if i%2==0:
-        ground.append(int(sys.stdin.readline().strip()))
+        ground[size]+=1
     else:
-        sky.append(int(sys.stdin.readline().strip()))
+        cell[size]+=1
 
-sky.sort()
-ground.sort()
-ans=0; count=0
+for i in range(h-1,0,-1):
+    ground[i] += ground[i+1]
+    cell[i] += cell[i+1]
 
-res=[]
+min_num=n
+min_count=0
+
 for i in range(1,h+1):
-    # 석순 이진탐색
-    count = 0
-    start1 = 0
-    end1 = len(ground)-1
-    res1 = len(ground)
-    while (start1<=end1):
-        mid = (start1 + end1) // 2
-        if ground[mid] >= i:
-            res1 = mid
-            end1 =mid-1
-        else :
-            start1 = mid+1
-    count+=(len(ground)-res1)
-
-    # 종유석 이진탐색
-    start2=0
-    end2=len(sky)-1
-    res2=len(sky)
-    while (start2<=end2):
-        mid =(start2+end2)//2
-        if h-sky[mid]<i:
-            res2=mid
-            end2 = mid - 1
-        else:
-            start2 = mid + 1
-    count+=(len(sky)-res2)
-    res.append(count)
-
-k=min(res)
-print(k, res.count(k))
+    if min_num > ground[i] + cell[h-i+1]:
+        min_num = ground[i] + cell[h-i+1]
+        min_count = 1
+    elif min_num == ground[i] + cell[h-i+1]:
+        min_count+=1
+print(min_num, min_count)
