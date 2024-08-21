ca3f8235fc9cacd17233d7381a9fc8d9d0c6076d
yhyeonseo <154505487+franktome@users.noreply.github.com>
2024-08-21 20:23:26 +0900

    [Gold V] Title: 개똥벌레, Time: 148 ms, Memory: 118344 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.py
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -1,28 +1,28 @@
-# 누적합을 이용한 풀이
 import sys
 
 n,h = map(int,sys.stdin.readline().split())
 
-ground=[0]*(h+1)
-cell = [0]*(h+1)
+ground=[0] * (h+1)
+cell = [0] * (h+1)
+
 for i in range(n):
-    size = int(sys.stdin.readline())
+    size=int(sys.stdin.readline())
     if i%2==0:
         ground[size]+=1
     else:
         cell[size]+=1
 
 for i in range(h-1,0,-1):
-    ground[i] += ground[i+1]
-    cell[i] += cell[i+1]
+    ground[i]+=ground[i+1]
+    cell[i]+=cell[i+1]
 
 min_num=n
 min_count=0
-
 for i in range(1,h+1):
-    if min_num > ground[i] + cell[h-i+1]:
-        min_num = ground[i] + cell[h-i+1]
-        min_count = 1
-    elif min_num == ground[i] + cell[h-i+1]:
+    if  min_num > ground[i]+cell[h-i+1]:
+        min_num=ground[i]+cell[h-i+1]
+        min_count=1
+    elif min_num == ground[i]+cell[h-i+1]:
         min_count+=1
+
 print(min_num, min_count)
