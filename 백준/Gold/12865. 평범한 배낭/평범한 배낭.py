ba19f1acb1dfa9edf8eb0ffef223519e9dbf6b9b
yhyeonseo <154505487+franktome@users.noreply.github.com>
2024-08-25 13:42:29 +0900

    [Gold V] Title: 평범한 배낭, Time: 328 ms, Memory: 189804 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,22 +1,14 @@
 import sys
 
-N, K = map(int, input().split())
-stuff = [[0,0]]
-knapsack = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
+n, k = map(int,sys.stdin.readline().split())
+bag = [list(map(int,sys.stdin.readline().split()) )for _ in range(n)]
+dp = [[0]*(k+1) for _ in range(n+1)]
 
-for _ in range(N):
-    stuff.append(list(map(int, input().split())))
-
-
-#냅색 문제 풀이
-for i in range(1, N + 1):
-    for j in range(1, K + 1):
-        weight = stuff[i][0] 
-        value = stuff[i][1]
-       
-        if j < weight:
-            knapsack[i][j] = knapsack[i - 1][j] #weight보다 작으면 위의 값을 그대로 가져온다
+for i in range(1,n+1):   # n은 물건을 꺼내기 위한 인덱스
+    for j in range(1,k+1):  # k는 dp table을 관리하기 위한 인덱스
+        if j>= bag[i-1][0]:  # 0은 무게, 1은 가치
+            dp[i][j] = max(bag[i-1][1]+dp[i-1][j-bag[i-1][0]], dp[i-1][j])
         else:
-            knapsack[i][j] = max(value + knapsack[i - 1][j - weight], knapsack[i - 1][j])
+            dp[i][j] = dp[i-1][j]
 
-print(knapsack[N][K])
+print(dp[n][k])
