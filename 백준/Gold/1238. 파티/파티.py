faefe634be9de5904e2481a534e98b6967883d75
yhyeonseo <154505487+franktome@users.noreply.github.com>
2024-09-23 19:32:14 +0900

    [Gold III] Title: 파티, Time: 1056 ms, Memory: 119572 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -8,27 +8,29 @@ graph = [[] for _ in range(n+1)]
 
 for i in range(m):
     a,b,c = map(int, input().split())
-    graph[a].append((b,c))
+    graph[a].append((c,b))
 
-def dijkstra(start,end):
-    distance = [inf] * (n + 1)
+
+def dijkstra(start, end):
+    distance = [inf]*(n+1)
     distance[start]=0
     q=[]
     heapq.heappush(q,(0,start))
+
     while(q):
         dist, now = heapq.heappop(q)
-        if distance[now] < dist:
+        if distance[now]<dist:
             continue
         for i in graph[now]:
-            cost = dist+i[1]
-            if cost<distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q,(cost,i[0]))
+            cost = dist + i[0]
+            if cost< distance[i[1]]:
+                distance[i[1]] = cost
+                heapq.heappush(q,(cost,i[1]))
     return distance[end]
 
 costs=[]
 for i in range(1,n+1):
-    go = dijkstra(i,x)
+    go=dijkstra(i,x)
     back = dijkstra(x,i)
     costs.append(go+back)
 print(max(costs))
