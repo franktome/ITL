c0eae6c25e31e20b3a9a08a37cb0ce84686521a9
yhyeonseo <154505487+franktome@users.noreply.github.com>
2024-10-03 14:59:01 +0900

    [Gold V] Title: 평범한 배낭, Time: 308 ms, Memory: 189784 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,14 +1,19 @@
 import sys
 
 n, k = map(int,sys.stdin.readline().split())
-bag = [list(map(int,sys.stdin.readline().split()) )for _ in range(n)]
+bag=[[0,0]]
+for i in range(n):
+    bag.append(list(map(int, sys.stdin.readline().split())))
 dp = [[0]*(k+1) for _ in range(n+1)]
+# d[n][k]는 N번째 물건 까지 살펴보았을 때, 무게가 K인 배낭의 최대 가치 이다.
+# 첫번째 index는 물건의 순번, 두번재 index는 물건의 가치이다.
 
-for i in range(1,n+1):   # n은 물건을 꺼내기 위한 인덱스
-    for j in range(1,k+1):  # k는 dp table을 관리하기 위한 인덱스
-        if j>= bag[i-1][0]:  # 0은 무게, 1은 가치
-            dp[i][j] = max(bag[i-1][1]+dp[i-1][j-bag[i-1][0]], dp[i-1][j])
+for i in range(1,n+1):
+    for j in range(1,k+1):
+        v = bag[i][1] #  i번째 물건의 가치
+        w = bag[i][0] #  i번째 물건의 무게
+        if j >= w:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
         else:
             dp[i][j] = dp[i-1][j]
-
 print(dp[n][k])
